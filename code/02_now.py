a8b7da311afa5a1aa1fffe62d7dc130a4d5cebeb
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-20 18:46:58 +0100

    Update 02_now.py

--- code/02_now.py
+++ code/02_now.py
@@ -1,7 +1,6 @@
 import requests
 import re
 
-# URL der Webseite, die den Live-Stream enthält
 url = "https://www.nowtv.com.tr/canli-yayin"
 
 def fetch_website_content(url):
@@ -12,38 +11,22 @@ def fetch_website_content(url):
         print("Failed to fetch the website content.")
         return None
 
-def extract_live_url(content):
-    match = re.search(r"daionUrl\s*:\s*'(https://nowtv\.daioncdn\.net/nowtv/nowtv\.m3u8\?ce=3&app=[^']+)'", content)
+def extract_dai_url(content):
+    # Aktualisiere den regulären Ausdruck, um die daiUrl von der neuen Seite zu extrahieren
+    match = re.search(r"daiUrl\s*:\s*'(https://nowtv-live-ad\.ercdn\.net/nowtv/playlist\.m3u8\?st=[^']+)'", content)
     if match:
         return match.group(1)
     else:
-        print("Live URL not found in the content.")
+        print("daiUrl not found in the content.")
         return None
-
-def create_m3u8_content(live_url):
-    # URLs für verschiedene Auflösungen definieren
-    m3u8_content = [
-        "#EXTM3U",
-        "#EXT-X-VERSION:3",
-        "#EXT-X-STREAM-INF:BANDWIDTH=1050000,AVERAGE-BANDWIDTH=950000,RESOLUTION=1280x720",
-        f"{live_url}",
-        "#EXT-X-STREAM-INF:BANDWIDTH=800000,AVERAGE-BANDWIDTH=700000,RESOLUTION=854x480",
-        f"{live_url}",
-        "#EXT-X-STREAM-INF:BANDWIDTH=550000,AVERAGE-BANDWIDTH=500000,RESOLUTION=640x360",
-        f"{live_url}"
-    ]
-    return "\n".join(m3u8_content)
 
 def main():
     site_content = fetch_website_content(url)
     if site_content:
-        live_url = extract_live_url(site_content)
-        if live_url:
-            print(live_url)  # Ausgabe der Live-URL zur Überprüfung
-            m3u8_content = create_m3u8_content(live_url)
-            with open("output/02_now.m3u8", "w") as f:
-                f.write(m3u8_content)
-            print(m3u8_content)
+        dai_url = extract_dai_url(site_content)
+        if dai_url:
+            print("Extracted daiUrl:", dai_url)
+            # Ausgabe der daiUrl
 
 if __name__ == "__main__":
     main()
