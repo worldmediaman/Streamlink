0e8743bbec489a84b1303c126ab191eb765c01dc
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-20 22:09:02 +0100

    Update 02_now.py

--- code/02_now.py
+++ code/02_now.py
@@ -14,28 +14,30 @@ def fetch_website_content(url):
 
 def extract_dai_url(content):
     # Regulärer Ausdruck, um die daiUrl von der neuen Seite zu extrahieren
-    match = re.search(r"daiUrl\s*:\s*'(https://nowtv-live-ad\.ercdn\.net/nowtv/nowtv_720p\.m3u8\?[^']+)'", content)
+    match = re.search(r"daiUrl\s*:\s*'(https://nowtv-live-ad\.ercdn\.net/nowtv/playlist\.m3u8\?st=[^']+)'", content)
     if match:
         return match.group(1)
     else:
         print("daiUrl not found in the content.")
         return None
 
-def fetch_and_save_url_content(dai_url):
-    response = requests.get(dai_url)
-    if response.status_code == 200:
-        with open("output/fetched_content.m3u8", "w") as f:
-            f.write(response.text)
-        print("Fetched content saved to 'output/fetched_content.m3u8'")
-    else:
-        print("Failed to fetch the content from the URL.")
+def create_m3u8_content(dai_url):
+    m3u8_content = [
+        "#EXTM3U",
+        "#EXTINF:-1, NOW",
+        dai_url
+    ]
+    return "\n".join(m3u8_content)
 
 def main():
     site_content = fetch_website_content(url)
     if site_content:
         dai_url = extract_dai_url(site_content)
         if dai_url:
-            fetch_and_save_url_content(dai_url)
+            m3u8_content = create_m3u8_content(dai_url)
+            with open("output/02_now.m3u8", "w") as f:
+                f.write(m3u8_content)
+            print(m3u8_content)
 
 if __name__ == "__main__":
     main()
