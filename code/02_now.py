b6bd9332826666ce6d89e4cce6743fd8830d0929
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-20 21:00:30 +0100

    Update 02_now.py

--- code/02_now.py
+++ code/02_now.py
@@ -1,21 +1,43 @@
 import requests
 import re
 
-# URL der Datei auf GitHub
-url = "https://raw.githubusercontent.com/worldmediaman/Streamlink/main/output/02_now.m3u8"
+# URL der Webseite, die den Live-Stream enthält
+url = "https://www.nowtv.com.tr/canli-yayin"
 
-def fetch_file_content(url):
+def fetch_website_content(url):
     response = requests.get(url)
     if response.status_code == 200:
         return response.text
     else:
-        print("Failed to fetch the file content.")
+        print("Failed to fetch the website content.")
         return None
 
+def extract_dai_url(content):
+    # Regulärer Ausdruck, um die daiUrl von der neuen Seite zu extrahieren
+    match = re.search(r"daiUrl\s*:\s*'(https://nowtv-live-ad\.ercdn\.net/nowtv/playlist\.m3u8\?st=[^']+)'", content)
+    if match:
+        return match.group(1)
+    else:
+        print("daiUrl not found in the content.")
+        return None
+
+def create_m3u8_content(dai_url):
+    m3u8_content = [
+        "#EXTM3U",
+        "#EXTINF:-1, NOW",
+        dai_url
+    ]
+    return "\n".join(m3u8_content)
+
 def main():
-    file_content = fetch_file_content(url)
-    if file_content:
-        print(file_content)
+    site_content = fetch_website_content(url)
+    if site_content:
+        dai_url = extract_dai_url(site_content)
+        if dai_url:
+            m3u8_content = create_m3u8_content(dai_url)
+            with open("output/02_now.m3u8", "w") as f:
+                f.write(m3u8_content)
+            print(m3u8_content)
 
 if __name__ == "__main__":
     main()
