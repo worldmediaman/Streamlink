16b8ec40bebaa2cee3ee8ebf597109c66b75a06f
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-20 22:35:29 +0100

    Update 03_Star.py

--- code/03_Star.py
+++ code/03_Star.py
@@ -2,22 +2,19 @@ import requests
 import re
 
 base_url = "https://mn-nl.mncdn.com/dogusdyg_star/"
-url = "https://www.startv.com.tr/canli-izle"
+url = "https://www.startv.com.tr/canli-yayin"
 
 def fetch_website_content(url):
     response = requests.get(url)
     if response.status_code == 200:
-        print("Website content fetched successfully.")
         return response.text
     else:
         print("Failed to fetch the website content.")
         return None
 
 def extract_live_url(content):
-    print("Extracting live URL from content...")
     match = re.search(r'liveUrl = \'(.*?)\'', content)
     if match:
-        print("Live URL found:", match.group(1))
         return match.group(1)
     else:
         print("Live URL not found in the content.")
@@ -26,14 +23,12 @@ def extract_live_url(content):
 def fetch_stream_content(url):
     response = requests.get(url)
     if response.status_code == 200:
-        print("Stream content fetched successfully.")
         return response.text
     else:
         print("Failed to fetch content.")
         return None
 
 def modify_content(content, base_url):
-    print("Modifying stream content...")
     lines = content.split("\n")
     modified_content = ""
     for line in lines:
@@ -52,7 +47,6 @@ def main():
             stream_content = fetch_stream_content(live_url)
             if stream_content:
                 modified_content = modify_content(stream_content, base_url)
-                print("Modified Content:")
                 print(modified_content)
 
 if __name__ == "__main__":
