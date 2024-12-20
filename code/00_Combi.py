cdbcaa0242ce200fb3742964ac53d67fc6ebc187
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-20 21:54:33 +0100

    Update 00_Combi.py

--- code/00_Combi.py
+++ code/00_Combi.py
@@ -1,55 +1,24 @@
-import requests
-import re
+def read_m3u8_file(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
 
-# URL der Webseite, die den Live-Stream enthält
-url = "https://www.nowtv.com.tr/canli-yayin"
+def merge_m3u8_files(file_paths, output_path):
+    merged_content = "#EXTM3U\n"
+    for file_path in file_paths:
+        content = read_m3u8_file(file_path)
+        # Entferne die erste Zeile, wenn es sich um "#EXTM3U" handelt
+        if content.startswith("#EXTM3U"):
+            content = content.split('\n', 1)[1]
+        merged_content += content + "\n"
 
-def fetch_website_content(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        print("Failed to fetch the website content.")
-        return None
-
-def extract_dai_url(content):
-    # Regulärer Ausdruck, um die daiUrl von der neuen Seite zu extrahieren
-    match = re.search(r"daiUrl\s*:\s*'(https://nowtv-live-ad\.ercdn\.net/nowtv/playlist\.m3u8\?st=[^']+)'", content)
-    if match:
-        return match.group(1)
-    else:
-        print("daiUrl not found in the content.")
-        return None
-
-def create_m3u8_content(dai_url):
-    m3u8_content = [
-        "#EXTM3U",
-        "#EXTINF:-1, NOW",
-        dai_url
-    ]
-    return "\n".join(m3u8_content)
-
-def fetch_and_save_url_content(dai_url):
-    response = requests.get(dai_url)
-    if response.status_code == 200:
-        with open("output/fetched_content.m3u8", "w") as f:
-            f.write(response.text)
-        print("Fetched content saved to 'output/fetched_content.m3u8'")
-    else:
-        print("Failed to fetch the content from the URL.")
+    with open(output_path, "w") as output_file:
+        output_file.write(merged_content)
+    print(f"Merged content saved to '{output_path}'")
 
 def main():
-    site_content = fetch_website_content(url)
-    if site_content:
-        dai_url = extract_dai_url(site_content)
-        if dai_url:
-            m3u8_content = create_m3u8_content(dai_url)
-            with open("output/02_now.m3u8", "w") as f:
-                f.write(m3u8_content)
-            print(m3u8_content)
-            
-            # Rufe den Inhalt der generierten URL ab und speichere ihn
-            fetch_and_save_url_content(dai_url)
+    file_paths = ["output/01_estar.m3u8", "output/02_now.m3u8"]
+    output_path = "output/combined_playlist.m3u8"
+    merge_m3u8_files(file_paths, output_path)
 
 if __name__ == "__main__":
     main()
