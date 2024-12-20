7e1fea03010731821709ca2c1fecfa3a0c17af52
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-20 18:09:14 +0100

    Update 02_now.py

--- code/02_now.py
+++ code/02_now.py
@@ -12,13 +12,13 @@ def fetch_website_content(url):
         print("Failed to fetch the website content.")
         return None
 
-def extract_daion_url(content):
-    # Aktualisiere den regulären Ausdruck, um die daionURL von der neuen Seite zu extrahieren
-    match = re.search(r'(https://nowtv\.daioncdn\.net/nowtv/nowtv\.m3u8\?ce=3&app=mobile_web&st=[^\'"]+)', content)
+def extract_live_url(content):
+    # Aktualisiere den regulären Ausdruck, um die Live-URL von der neuen Seite zu extrahieren
+    match = re.search(r'(https://nowtv\.daioncdn\.net/nowtv/nowtv\.m3u8\?ce=3&app=[^\'"]+)', content)
     if match:
         return match.group(1)
     else:
-        print("daionURL not found in the content.")
+        print("Live URL not found in the content.")
         return None
 
 def fetch_stream_content(url):
@@ -29,30 +29,26 @@ def fetch_stream_content(url):
         print("Failed to fetch content.")
         return None
 
-def create_m3u8_content(base_url):
-    m3u8_content = [
-        "#EXTM3U",
-        "#EXT-X-VERSION:3",
-        "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=3000000,RESOLUTION=1920x1080",
-        f"{base_url}nowtv_1080p.m3u8",
-        "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1500000,RESOLUTION=1280x720",
-        f"{base_url}nowtv_720p.m3u8",
-        "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=900000,RESOLUTION=854x480",
-        f"{base_url}nowtv_480p.m3u8"
-    ]
-    return "\n".join(m3u8_content)
+def modify_content(content):
+    lines = content.split("\n")
+    modified_content = ""
+    for line in lines:
+        if line.startswith("nowtv_"):
+            full_url = line
+            modified_content += full_url + "\n"
+        else:
+            modified_content += line + "\n"
+    return modified_content
 
 def main():
     site_content = fetch_website_content(url)
     if site_content:
-        daion_url = extract_daion_url(site_content)
-        if daion_url:
-            print(daion_url)  # Ausgabe der daionURL
-            base_url = re.match(r'(https://nowtv\.daioncdn\.net/nowtv/)', daion_url).group(1)
-            m3u8_content = create_m3u8_content(base_url)
-            with open("output/02_now.m3u8", "w") as f:
-                f.write(m3u8_content)
-            print(m3u8_content)
+        live_url = extract_live_url(site_content)
+        if live_url:
+            stream_content = fetch_stream_content(live_url)
+            if stream_content:
+                modified_content = modify_content(stream_content)
+                print(modified_content)
 
 if __name__ == "__main__":
     main()
